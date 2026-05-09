460afb591b14d46fba6a7313ad0aa3a411d1975b
Moises Nunes <33706215+moisesnunes@users.noreply.github.com>
2026-05-09 18:56:19 -0300

    Update interconnect.h

--- interconnect.h
+++ interconnect.h
@@ -3,19 +3,22 @@
 
 #include "bios.h"
 #include "ram.h"
+#include "gpu.h"
 
 typedef struct
 {
     Bios bios;
-    Ram ram;
+    Ram  ram;
+    Gpu  gpu;
 } Interconnect;
 
-void inter_init(Interconnect *inter, Bios bios);
-uint32_t inter_load32(const Interconnect *inter, uint32_t addr);
-uint16_t inter_load16(const Interconnect *inter, uint32_t addr);
-uint8_t inter_load8(const Interconnect *inter, uint32_t addr);
-void inter_store32(Interconnect *inter, uint32_t addr, uint32_t val);
-void inter_store16(Interconnect *inter, uint32_t addr, uint16_t val);
-void inter_store8(Interconnect *inter, uint32_t addr, uint8_t val);
+void     inter_init(Interconnect *inter, Bios bios);
+void     inter_destroy(Interconnect *inter);
+uint32_t inter_load32(Interconnect *inter, uint32_t addr);
+uint16_t inter_load16(Interconnect *inter, uint32_t addr);
+uint8_t  inter_load8(Interconnect *inter, uint32_t addr);
+void     inter_store32(Interconnect *inter, uint32_t addr, uint32_t val);
+void     inter_store16(Interconnect *inter, uint32_t addr, uint16_t val);
+void     inter_store8(Interconnect *inter, uint32_t addr, uint8_t val);
 
 #endif /* PSX_INTERCONNECT_H */
